99d142dcc65fb6ee121cdc007fc500ed399d5773
tamim11 <mmmtamim@gmail.com>
2021-02-07 19:53:54 +0600

    second commit

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const inputField = document.getElementById("input-field");
 const button = document.getElementById("btn");
 const mainSection = document.getElementById("main");
+const detailsSection = document.getElementById("details-section");
 
 // functions
+
+const displayFoodDetails = id => {
+    const urlId = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+    fetch(urlId)
+        .then(res => res.json())
+        .then(data => {
+            detailsSection.innerHTML = "";
+            const meal = data["meals"][0];
+            const detailsDiv = document.createElement("div");
+            const detailsInfo = `
+            <img src="${meal.strMealThumb}" alt="pic">
+            <h2>${meal.strMeal}</h2>
+            <h4>Instructions</h4>
+            <p>${meal.strInstructions}</p>
+        `;
+            detailsDiv.innerHTML = detailsInfo;
+            detailsSection.appendChild(detailsDiv);
+        });
+}
+
 const process = data => {
-    mainSection.innerHTML = "";
+    mainSection.innerHTML = ""; // this line clears previously searched items
     const meals = data["meals"];
     meals.forEach(meal => {
         const newDiv = document.createElement("div");
@@ -12,7 +33,8 @@ const process = data => {
         const mealInfo = `
             <img src="${meal.strMealThumb}" alt="pic">
             <h3>${meal.strMeal}</h3>
-        `
+            <button onclick='displayFoodDetails("${meal.idMeal}")'>Details</button>
+        `;
         newDiv.innerHTML = mealInfo;
         mainSection.appendChild(newDiv);
     });
@@ -26,3 +48,4 @@ button.addEventListener('click', () => {
         .then(response => response.json())
         .then(data => process(data));
 });
+
